8a0e70657a3b6d5abc4479d1a0a2bc195bfa436a
usiq8 <151259866+usiq8@users.noreply.github.com>
2024-08-19 16:32:47 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css" >
+    <title>Document</title>  
+</head>
+<body>
+    
+<div class="centrar_main">
+    <main>
+<header>
+        <div class="form-group mb-3">
+            <img src="avatar-jessica.jpeg" alt="Avatar" class="circle-image">
+        </div>
+
+        <div class="textos">
+            <h4>Jessica Randall</h4>
+            <h5>London, United Kingdom</h5>
+            <br>
+            <h6>"Front-end developer and avid reader."</h6>
+        </div>
+</header>
+
+        <br>
+        
+        <ul>
+            <li class="form-group mb-3">
+                <a href="https://github.com/" target="_blank">GitHub</a>
+            </li>
+            <li class="form-group mb-3">
+                <a href="https://www.frontendmentor.io/" target="_blank">Frontend Mentor</a>
+            </li>
+            <li class="form-group mb-3">
+                <a href="https://es.linkedin.com/" target="_blank">LinkedIn</a>
+            </li>
+            <li class="form-group mb-3">
+                <a href="https://x.com/" target="_blank">Twitter</a>
+            </li>
+            <li class="form-group mb-3">
+                <a href="https://www.instagram.com/" target="_blank">Instagram</a>
+            </li>
+        </ul>
+
+    </main>
+</div>
+
+</body>
+</html>
